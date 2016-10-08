2d00eefc813614525edacc38fa431b54edea30db
Marek Grygorczuk <marek@grygorczuk.pl>
2016-10-08 23:11:13 +0200

    Changed DefaultController

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,22 +2,45 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Dto\NewRideDto;
+use AppBundle\Entity\User;
+use AppBundle\Repository\DatabaseRideRepository;
+use AppBundle\Repository\MemoryRideRepository;
+use AppBundle\Service\DrafterService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        $dto = new NewRideDto();
+        $dto->user = new User();
+        $dto->rideLocation = 'warszawa';
+        $dto->rideBeginning = new \DateTime();
+
+        $drafterService = new DrafterService(new MemoryRideRepository());
+        $drafterService->addRide($dto);
+
+        $allRides = json_encode($drafterService->AllRides());
+
+
+        return new Response(
+            '<html><body>
+            <h1>Drafter</h1>
+            <p>'.$allRides.'</p>
+            </body></html>'
+
+        );
+
+
+//        return $this->render('default/index.html.twig', [
+//            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+//        ]);
     }
 }
