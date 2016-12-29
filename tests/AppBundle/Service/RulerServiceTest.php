f72644a1df0c96610bd3e7c10555ae085b9c3a6a
mocniak <karol@mocniak.pl>
2016-12-29 22:41:06 +0100

    - code realignment

--- tests/AppBundle/Service/RulerServiceTest.php
+++ tests/AppBundle/Service/RulerServiceTest.php
@@ -2,7 +2,6 @@
 
 namespace tests\AppBundle\Service;
 
-
 use AppBundle\Service\GpsLocation;
 use AppBundle\Service\RulerService;
 
@@ -14,9 +13,9 @@ class RulerServiceTest extends \PHPUnit_Framework_TestCase
         $cracow = new GpsLocation(50, 20);
         $zero = new GpsLocation(0, 0);
         $london = new GpsLocation(51.5, 0);
-        $this->assertEquals(1412, $ruler->getDistance($cracow,$london),'',1.0);
-        $this->assertEquals(5727, $ruler->getDistance($zero,$london),'',1.0);
-        $this->assertEquals(5876, $ruler->getDistance($cracow,$zero),'',1.0);
+        $this->assertEquals(1412, $ruler->getDistance($cracow, $london), '', 1.0);
+        $this->assertEquals(5727, $ruler->getDistance($zero, $london), '', 1.0);
+        $this->assertEquals(5876, $ruler->getDistance($cracow, $zero), '', 1.0);
     }
 
 }
